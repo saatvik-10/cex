5668d8c82f70963a3dad5d540c9263d9f5565331
saatvik-10 <saatvikmadan529@gmail.com>
2026-08-25 11:34:39 +0530

    added app state mutex

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::sync::Mutex;
+
+use crate::{
+    routes::{sign_in, sign_up},
+    types::user::User,
+};
+use actix_web::{App, HttpServer, web};
+
+mod routes;
+mod types;
+
+struct AppState {
+    user_index: Mutex<u32>,
+    users: Mutex<Vec<User>>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let app_state = web::Data::new(AppState {
+        users: Mutex::new(vec![]),
+        user_index: Mutex::new(0),
+    });
+
+    HttpServer::new(|| {
+        App::new()
+            .app_data(app_state)
+            .service(sign_in)
+            .service(sign_up)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
+}
