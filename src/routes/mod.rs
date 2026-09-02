400341212fccdb894e798deb6bbf316ca9caae4d
saatvik-10 <saatvikmadan529@gmail.com>
2026-09-02 15:54:37 +0530

    routed mod

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,1 +1,18 @@
-pub mod user;
+use actix_web::web;
+
+use crate::handlers::{user, wallet};
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/auth")
+            .service(user::sign_up)
+            .service(user::sign_in)
+            .service(user::refresh_tokens)
+            .service(user::profile),
+    )
+    .service(
+        web::scope("/wallet")
+            .service(wallet::get_balance)
+            .service(wallet::onramp),
+    );
+}
