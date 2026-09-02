c8a527236885f761f3889f44f7eb08041b9c3c11
saatvik-10 <saatvikmadan529@gmail.com>
2026-09-02 13:25:18 +0530

    updated user return types

--- src/types/user.rs
+++ src/types/user.rs
@@ -1,29 +1,28 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-pub struct User {
-    pub id: u32,
+/// Non-sensitive user info returned to clients.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UserSummary {
+    pub id: Uuid,
     pub username: String,
-    pub password: String,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct SignInInput {
-    pub username: String,
-    pub password: String,
-}
-
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct SignUpInput {
     pub username: String,
     pub password: String,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct SignInRes {
-    pub msg: String,
+#[derive(Debug, Deserialize)]
+pub struct SignInInput {
+    pub username: String,
+    pub password: String,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct SignUpRes {
-    pub msg: String,
+#[derive(Debug, Serialize)]
+pub struct AuthResponse {
+    pub access_token: String,
+    pub refresh_token: String,
+    pub user: UserSummary,
 }
