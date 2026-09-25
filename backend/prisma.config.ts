206ba9025d6f713ecf113d671dc53751368e1b2f
saatvik-10 <saatvikmadan529@gmail.com>
2026-09-25 16:24:46 +0530

    packages and configs

---
+++ backend/prisma.config.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+  migrate: {
+    adapter: () =>
+      new PrismaPg({ connectionString: process.env.DATABASE_URL! }),
+  },
+});
